be5549c1993b03bba8054cf70ac3ec7055dcb297
James McCutcheon <j.mccutcheon@uit.no>
2023-02-16 18:28:52 +0000

    Progress on batch lowres.py

--- fosquant/run_lowres.py
+++ fosquant/run_lowres.py
@@ -5,11 +5,8 @@ import getopt
 import os
 import subprocess
 import json
-import logging
 
-from datetime import datetime
-
-import pandas as pd
+from helper_fx import *
 
 sys.path.append("~/Github/fosquant/")
 
@@ -17,6 +14,7 @@ sys.path.append("~/Github/fosquant/")
 def parse_args(argv, config_data):
     args_dict = {}
     args_dict["animals"] = ""
+    args_dict["project_dir"] = config_data["project_dir"]
     args_dict["channels"] = config_data["channels"]
     args_dict["rotate"] = config_data["rotate"]
     args_dict["invert"] = config_data["invert"] 
@@ -44,12 +42,54 @@ def parse_args(argv, config_data):
     
     return args_dict
 
-f = open("../config_hires.json")
+f = open("../config_lowres.json")
 config_data = json.load(f)
 args_dict = parse_args(sys.argv, config_data)
 
 print(config_data)
 print(args_dict)
 
+folder = config_data["project_dir"]
+logger = setup_logger(args_dict["project_dir"])
+os.chdir(folder)
 
-folder = config_data["path_to_project_dir"]
+if args_dict["animals"] == "all":
+    args_dict["animals"] = [d for d in os.listdir(folder) if config_data["prefix"] in d]
+elif args_dict["animals"] == "":
+    logger.info("No animals given. Exiting")
+    sys.exit(2)
+else:
+    args_dict["animals"] = args_dict["animals"].split()
+
+print(args_dict["animals"])
+
+for animal in args_dict["animals"]:
+    try:
+        os.chdir(os.path.join(folder, animal))
+    except FileNotFoundError:
+        logger.info("No folder corresponds to {}. Nothing to transfer.".format(animal))
+        continue
+    
+    if "rawdata" not in os.listdir("."):
+        logger.info("No raw data folder for {}. Nothing to transfer.".format(animal))
+        continue
+
+    os.chdir(os.path.join(".", "rawdata"))
+
+    vsi_files = [vsi for vsi in os.listdir(".") if vsi.endswith(".vsi")]
+    print(vsi_files)
+
+    for vsi in vsi_files:
+        print(vsi.split(".")[0])
+        # subprocess.call({} "")
+    # if "rawdata" not in os.listdir("."):
+    #     logger.info("No raw data folder for {}. Nothing to transfer.".format(animal))
+    #     continue
+    
+    
+#     path_to_azcopy = config_data["path_to_azcopy"]
+#     lowres_local = os.path.join(".", "lowres")
+#     lowres_remote = os.path.join(config_data["remote_lowres"], animal)
+
+#     logger.info("Transferring lowres files from VM to Azure for {}".format(animal))
+#     subprocess.call("{} cp {} {} --recursive=true".format(path_to_azcopy, lowres_local, lowres_remote), shell=True)
