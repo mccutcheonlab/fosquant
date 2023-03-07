b25e0ddb54948f2f4160688e45296912f0d568df
James McCutcheon <j.mccutcheon@uit.no>
2023-03-07 10:57:58 +0000

    added check masks

--- fosquant/check_integrity.py
+++ fosquant/check_integrity.py
@@ -98,6 +98,37 @@ def check_hires(project_dir, logger, rois=None):
     # look for pngs, if none alert and move on, if some check that all exist
     # look for cellpose output
 
+def check_masks(project_dir, logger):
+
+    logger.info("Checking whether PNG masks exist...")
+
+    hires_dir = os.path.join(project_dir, "hires")
+    if not os.path.exists(hires_dir):
+        logger.warning("No hires folder exists in {}".format(project_dir))
+        return False
+    
+    chans = [folder for folder in os.listdir(hires_dir) if "chan" in folder]
+
+    for chan in chans:
+        chan_dir = os.path.join(hires_dir, chan)
+        png_files = [png for png in os.listdir(chan_dir) if (png.endswith(".png")) and ("masks" in png)] # need to ensure only masks files
+        if len(png_files) == 0:
+            logger.warning("No mask files found in {}".format(chan_dir))
+            return False
+        else:
+            logger.info("Found {} .png files".format(len(png_files)))
+            section_names = [s.split(".")[0].split("_")[-1] for s in png_files]
+            print(section_names)
+            if rois != None:
+                print(rois)
+                print(section_names)
+                if rois == section_names:
+                    logger.info("ROIs from ROI file match mask files in {}".format(chan_dir))
+                    return True
+                else:
+                    logger.warning("ROIs from ROI file DO NOT MATCH mask files in {}".format(chan_dir))
+                    return False
+
 if __name__ == "__main__":
     project_dir = "D:\\TestData\\fostrap\\FTIG\\FT108"
     project_dir = "/mnt/d/TestData/fostrap/FTIG/FT108"
@@ -109,9 +140,12 @@ if __name__ == "__main__":
     if rois != None:
         check_lowres(project_dir, logger, rois=rois)
         check_hires(project_dir, logger, rois=rois)
+        check_masks(project_dir, logger, rois=rois)
     else:
         check_lowres(project_dir, logger)
         check_hires(project_dir, logger)
+        check_masks(project_dir, logger)
+
 
 
 
