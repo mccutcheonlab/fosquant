1d5d429b2a0565dd0719dcfe9cbec27a6c83e5d8
jaimemcc <j.mccutcheon@uit.no>
2023-03-08 17:12:51 +0100

    Added features incl. size threshold

--- fosquant/run_roi_count.py
+++ fosquant/run_roi_count.py
@@ -5,10 +5,9 @@ import json
 
 from read_roi import read_roi_zip
 import numpy as np
-from skimage.draw import polygon, polygon2mask
-from skimage.io import imread, imshow
+from skimage.draw import polygon2mask
+from skimage.io import imread
 from skimage.measure import find_contours
-from matplotlib import pyplot as plt
 import pandas as pd
 
 from helper_fx import *
@@ -18,9 +17,11 @@ def parse_args(argv, config_data):
     args_dict = config_data
     args_dict["animals"] = ""
     args_dict["skip_integrity_check"] = False
+    args_dict["verbose"] = False
+    args_dict["overwrite"] = False
 
     try:
-        opts, args = getopt.getopt(argv[1:], "a:i")
+        opts, args = getopt.getopt(argv[1:], "a:ivo")
     except:
         print(arg_help)
         sys.exit(2)
@@ -32,7 +33,11 @@ def parse_args(argv, config_data):
         elif opt in ("-a", "--animals"):
             args_dict["animals"] = arg
         elif opt in ("-i", "--check_integrity"):
-            args_dict["skip_integrity_check"] = True 
+            args_dict["skip_integrity_check"] = True
+        elif opt in ("-v", "--verbose"):
+            args_dict["verbose"] = True
+        elif opt in ("-o", "--overwrite"):
+            args_dict["overwrite"] = True 
 
     print("Arguments parsed successfully")
     
@@ -53,25 +58,31 @@ def get_roi_coords(roi, scale_factor):
     
     return xy
 
-def count_neurons(im, roi_coords):
+def count_neurons(im, roi_coords, verbose=False):
     
     mask = polygon2mask(im.shape, roi_coords)
     masked_image = im * mask
     
     ncells = len(np.unique(masked_image)) - 1
 
-    print("The number of neurons is", ncells)
+    if verbose: print("The number of neurons is", ncells)
     
     return ncells, masked_image
 
-def get_coloc(im1, im2, roi_coords):
+def get_coloc(im1, im2, area_threshold = 8, verbose=False):
     binary_1 = np.where(im1 > 0, 1, 0)
     binary_2 = np.where(im2 > 0, 1, 0)
     coloc = binary_1 * binary_2
-    
-    return len(find_contours(coloc, 0.8))
 
-def process_rois(folder, animal, rois=[]):
+    contours = find_contours(coloc, 0.8)
+    areas = [np.sum(polygon2mask(coloc.shape,c)) for c in contours]
+    ncoloc = len([a for a in areas if a > area_threshold])
+
+    if verbose: print("The number of colocalised neurons is", ncoloc)
+    
+    return ncoloc
+
+def process_rois(folder, animal, rois=[], verbose=False):
     # set folder names
     hirespath = os.path.join(folder, animal, "hires")
     lowrespath = os.path.join(folder, animal, "lowres")
@@ -84,7 +95,7 @@ def process_rois(folder, animal, rois=[]):
     existing_section = ""
     for roi in roidata:
         section, region = parse_roi_name(roi)
-        print(roi)
+        if verbose: print(roi)
         
         if section != existing_section:
             png = [im for im in os.listdir(os.path.join(hirespath, "chan1")) if (section in im) and ("masks" in im)][0]
@@ -99,17 +110,14 @@ def process_rois(folder, animal, rois=[]):
         # make mask and get cell numbers
         xy = get_roi_coords(roidata[roi], scale_factor)
         
-        nfos, masked_fos = count_neurons(im_fos, xy)
-        ntrap, masked_trap = count_neurons(im_trap, xy)
-        ncoloc = get_coloc(masked_fos, masked_trap, xy)
+        nfos, masked_fos = count_neurons(im_fos, xy, verbose=verbose)
+        ntrap, masked_trap = count_neurons(im_trap, xy, verbose=verbose)
+        ncoloc = get_coloc(masked_fos, masked_trap, verbose=verbose)
         
         area = np.sum(polygon2mask(im_fos.shape, xy))
         
         section_data = {"animal": [animal], "section": [section], "region": [region], "area": [area], "nfos": [nfos], "ntrap": [ntrap], "ncoloc": [ncoloc]}
-        
-        df_temp = pd.DataFrame(section_data)
- 
-        features.append(df_temp)
+        features.append(pd.DataFrame(section_data))
         
     df = pd.concat(features, ignore_index=True)
 
@@ -138,18 +146,28 @@ print(args_dict["animals"])
 list_of_dfs = []
 for animal in args_dict["animals"]:
     existing_csv_path = os.path.join(folder, animal, "lowres", "user_rois_{}.csv".format(animal))
+
     if os.path.exists(existing_csv_path):
-        df = pd.read_csv(existing_csv_path)
-        if "mouse" in df.columns:
-            df.rename({"mouse": "animal"}, inplace=True)
-        list_of_dfs.append(df)
-    else:
+        if not args_dict["overwrite"]:
+            df = pd.read_csv(existing_csv_path)
+            if "mouse" in df.columns:
+                df.rename({"mouse": "animal"}, inplace=True)
+            list_of_dfs.append(df)
+            continue
+        else:
+            print("Overwriting existing csv file for {}".format(animal))
+    if not args_dict["skip_integrity_check"]:
         rois = check_rawdata(os.path.join(folder, animal), logger)
-        if check_masks(os.path.join(folder, animal), logger, rois=rois):
-            try:
-                list_of_dfs.append(process_rois(folder, animal))
-            except FileNotFoundError:
-                print("Files not available for {}".format(animal))
+        if not check_masks(os.path.join(folder, animal), logger, rois=rois):
+            print("Integrity check not passed. Not analysing {}".format(animal))
+            continue
+    else:
+        print("Skipping integrity check... Could be issues with analysis.")
+        
+    try:
+        list_of_dfs.append(process_rois(folder, animal))
+    except FileNotFoundError:
+        print("Files not available for {}".format(animal))
 
 df_main = pd.concat(list_of_dfs)
 
@@ -171,9 +189,9 @@ df_main.to_csv(os.path.join(results_folder, "df_user_counts.csv"))
 
 print(df_main.head())
 
-print(df_meta.head())
+# print(df_meta.head())
 
-print(df_main.head())
+# print(df_main.head())
 # df_main.to_csv(os.path.join(results_folder, "df_user_counts_with_groups.csv"))
 
 
