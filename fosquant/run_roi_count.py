9be97641d825409ad290d62a2e49e407891257b1
jaimemcc <j.mccutcheon@uit.no>
2023-09-12 11:50:57 +0200

    Major update to roi counting script

--- fosquant/run_roi_count.py
+++ fosquant/run_roi_count.py
@@ -2,6 +2,7 @@ import sys
 import getopt
 import os
 import json
+from pathlib import Path
 
 from read_roi import read_roi_zip
 import numpy as np
@@ -54,11 +55,26 @@ def parse_roi_name(roiname):
     
     return section, region
 
+def get_sections_from_rois(roidata):
+    sections = []
+    for roi in roidata.keys():
+        sections.append(roi.split("_")[0])
+ 
+    return list(set(sections))
+
 def get_roi_coords(roi, scale_factor):
 
-    y = [i*scale_factor for i in roi["x"]]
-    x = [i*scale_factor for i in roi["y"]]
-    xy = [(x,y) for x,y in zip(x,y)]
+    if roi["type"] == "polygon":
+        y = [i*scale_factor for i in roi["x"]]
+        x = [i*scale_factor for i in roi["y"]]
+        xy = [(x,y) for x,y in zip(x,y)]
+
+    elif roi["type"] == "rectangle":
+        
+        y1, y2 = [i*scale_factor for i in (roi["left"], roi["left"]+roi["width"])]
+        x1, x2 = [i*scale_factor for i in (roi["top"], roi["top"]+roi["height"])]
+
+        xy = [(x1,y1), (x2,y1), (x2,y2), (x1,y2)]
     
     return xy
 
@@ -88,115 +104,128 @@ def get_coloc(im1, im2, area_threshold = 8, verbose=False):
 
 def process_rois(folder, animal, rois=[], verbose=False):
     # set folder names
-    hirespath = os.path.join(folder, animal, "hires")
-    lowrespath = os.path.join(folder, animal, "lowres")
-    roipath = os.path.join(lowrespath, "{}_userdefined_ROIs.zip".format(animal))
+    hirespath = folder / animal / "hires"
+    lowrespath = folder / animal / "lowres"
+    roipath = lowrespath / "{}_userdefined_ROIs_cleaned.zip".format(animal)
 
     for path in [hirespath, lowrespath, roipath]:
-        if not os.path.exists(path): return
+        if not path.exists(): return
     
     # load in roi file
     roidata = read_roi_zip(roipath)   
     
     features = []
-    existing_section = ""
-    for roi in roidata:
-        section, region = parse_roi_name(roi)
-        if verbose: print(roi)
-        
-        if section != existing_section:
-            png = [im for im in os.listdir(os.path.join(hirespath, "chan1")) if (section in im) and ("masks" in im)][0]
-            lowres = [im for im in os.listdir(lowrespath) if (section in im)][0]
-            im_fos = imread(os.path.join(hirespath, "chan1", png))
-            im_trap = imread(os.path.join(hirespath, "chan2", png))
-            im_low = imread(os.path.join(lowrespath, lowres))
     
-            scale_factor = int(im_fos.shape[0]/im_low.shape[0])
-            existing_section = section
-            
-        # make mask and get cell numbers
-        xy = get_roi_coords(roidata[roi], scale_factor)
-        
-        nfos, masked_fos = count_neurons(im_fos, xy, verbose=verbose)
-        ntrap, masked_trap = count_neurons(im_trap, xy, verbose=verbose)
-        ncoloc = get_coloc(masked_fos, masked_trap, verbose=verbose)
-        
-        area = np.sum(polygon2mask(im_fos.shape, xy))
-        
-        section_data = {"animal": [animal], "section": [section], "region": [region], "area": [area], "nfos": [nfos], "ntrap": [ntrap], "ncoloc": [ncoloc]}
-        features.append(pd.DataFrame(section_data))
-        
+    for section in get_sections_from_rois(roidata):
+        print(f"Analyzing section {section} for {animal}")
+
+        png = [im for im in os.listdir(hirespath / "chan1") if (section in im) and ("masks" in im)][0]
+        lowres = [im for im in os.listdir(lowrespath) if (section in im)][0]
+        im_fos = imread(hirespath / "chan1" / png)
+        im_trap = imread(hirespath / "chan2" / png)
+        im_low = imread(lowrespath / lowres)
+
+        scale_factor = int(im_fos.shape[0] / im_low.shape[0])
+
+        for roi in roidata:
+            roi_section, region = parse_roi_name(roi)
+            if roi_section != section:
+                continue
+            print(animal, section, region)
+
+            xy = get_roi_coords(roidata[roi], scale_factor)
+
+            nfos, masked_fos = count_neurons(im_fos, xy, verbose=verbose)
+            ntrap, masked_trap = count_neurons(im_trap, xy, verbose=verbose)
+            ncoloc = get_coloc(masked_fos, masked_trap, verbose=verbose)
+
+            area = np.sum(polygon2mask(im_fos.shape, xy))
+
+            section_data = {"animal": [animal], "section": [section], "region": [region], "area": [area], "nfos": [nfos], "ntrap": [ntrap], "ncoloc": [ncoloc]}
+            features.append(pd.DataFrame(section_data))
+   
     df = pd.concat(features, ignore_index=True)
 
-    df.to_csv(os.path.join(lowrespath, "user_rois_{}.csv".format(animal)))
+    df.to_csv(lowrespath / "user_rois_{}.csv".format(animal))
+
+    print(f"Finished analyzing {animal}")
 
     return df
 
-f = open("../config.json")
-config_data = json.load(f)
-args_dict = parse_args(sys.argv, config_data)
+if __name__ == "__main__":
+    config_file = Path("../config.json").resolve()
+    f = open(config_file)
+    config_data = json.load(f)
+    args_dict = parse_args(sys.argv, config_data)
 
-folder = args_dict["project_dir"]
-logger = setup_logger(folder)
-os.chdir(folder)
+    folder = Path(args_dict["project_dir"])
+    logger = setup_logger(folder)
+    os.chdir(folder)
 
-if args_dict["animals"] == "all":
-    args_dict["animals"] = [d for d in os.listdir(folder) if args_dict["prefix"] in d]
-elif args_dict["animals"] == "":
-    logger.info("No animals given. Exiting")
-    sys.exit(2)
-else:
-    args_dict["animals"] = args_dict["animals"].split()
+    if args_dict["animals"] == "all":
+        args_dict["animals"] = [d for d in os.listdir(folder) if args_dict["prefix"] in d]
+    elif args_dict["animals"] == "":
+        logger.info("No animals given. Exiting")
+        sys.exit(2)
+    else:
+        args_dict["animals"] = args_dict["animals"].split()
 
-print(args_dict["animals"])
+    print(args_dict["animals"])
 
-list_of_dfs = []
-animals_to_process = []
+    list_of_dfs = []
+    animals_to_process = []
 
-for animal in args_dict["animals"]:
-    existing_csv_path = os.path.join(folder, animal, "lowres", "user_rois_{}.csv".format(animal))
+    for animal in args_dict["animals"]:
+        existing_csv_path = folder / animal / "lowres" / "user_rois_{}.csv".format(animal)
 
-    if os.path.exists(existing_csv_path):
-        if not args_dict["overwrite"]:
-            df = pd.read_csv(existing_csv_path)
-            if "mouse" in df.columns:
-                df.rename({"mouse": "animal"}, inplace=True)
-            list_of_dfs.append(df)
-            continue
+        if existing_csv_path.exists():
+            if not args_dict["overwrite"]:
+                df = pd.read_csv(existing_csv_path)
+                if "mouse" in df.columns:
+                    df.rename({"mouse": "animal"}, inplace=True)
+                list_of_dfs.append(df)
+                continue
+            else:
+                print("Overwriting existing csv file for {}".format(animal))
+        if not args_dict["skip_integrity_check"]:
+            check = Check(folder / animal, logger)
+            if not check.check_all():
+                logger.warning("Integrity check not passed. Not analysing {}".format(animal))
+                continue
         else:
-            print("Overwriting existing csv file for {}".format(animal))
-    if not args_dict["skip_integrity_check"]:
-        check = Check(os.path.join(folder, animal), logger)
-        if not check.check_all():
-            logger.warning("Integrity check not passed. Not analysing {}".format(animal))
-            continue
+            logger.warning("Skipping integrity check for {}. Could be issues with analysis.".format(animal))
+        
+        animals_to_process.append(animal)
+
+    logger.info("Animals being processed are: {}".format(animals_to_process))
+
+    if len(animals_to_process) > 0:
+        if args_dict["threaded"]:
+            pool_args = [(folder, animal) for animal in animals_to_process]
+
+            pool_size = cpu_count()
+            with Pool(processes=pool_size) as pool:
+                list_of_pooled_dfs = pool.starmap(process_rois, pool_args)
+        else:
+            list_of_pooled_dfs = []
+            for animal in animals_to_process:
+                list_of_pooled_dfs.append(process_rois(folder, animal, verbose=args_dict["verbose"]))
+
+        df_main = pd.concat(list_of_dfs+list_of_pooled_dfs)
     else:
-        logger.warning("Skipping integrity check for {}. Could be issues with analysis.".format(animal))
-    
-    animals_to_process.append(animal)
+        try:
+            df_main = pd.concat(list_of_dfs)
+        except:
+            print("Failed to concatenate. Maybe no dataframes. Exiting.")
+            sys.exit(2)
 
-logger.info("Animals being processed are: {}".format(animals_to_process))
 
-if len(animals_to_process) > 0:
-    if args_dict["threaded"]:
-        pool_args = [(folder, animal) for animal in animals_to_process]
+    results_folder = folder / "results"
+    if not results_folder.exists():
+        os.mkdir(results_folder)
 
-        pool_size = cpu_count()
-        with Pool(processes=pool_size) as pool:
-            list_of_pooled_dfs = pool.starmap(process_rois, pool_args)
-    else:
-        list_of_pooled_dfs = []
-        for animal in animals_to_process:
-             list_of_pooled_dfs.append(process_rois(folder, animal, verbose=args_dict["verbose"]))
+    df_main.to_csv(results_folder / "df_user_counts.csv")
 
-    df_main = pd.concat(list_of_dfs+list_of_pooled_dfs)
-else:
-    df_main = pd.concat(list_of_dfs)
+    print(df_main.head())
 
-results_folder = os.path.join(folder, "results")
-if not os.path.exists(results_folder):
-    os.mkdir(results_folder)
-
-df_main.to_csv(os.path.join(results_folder, "df_user_counts.csv"))
-
-print(df_main.head())
+    print("Finished running script successfully")
