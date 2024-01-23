d00a3a63edd9e0affcf9c60c26a147aafe948acb
James McCutcheon <j.mccutcheon@uit.no>
2024-01-23 11:32:22 +0000

    downloaddata script working nicely

--- fosquant/downloaddata.py
+++ fosquant/downloaddata.py
@@ -1,13 +1,13 @@
 # import sys
 # import getopt
-# import os
+
 # import shutil
 # import subprocess
 # import json
 
 # from datetime import datetime
 
-# import pandas as pd
+
 
 # from helper_fx import *
 
@@ -18,8 +18,11 @@ from pathlib import Path
 import click
 import json
 import subprocess
+from urllib.parse import urljoin
 
-from helper_fx import setup_logger
+import pandas as pd
+
+from helper_fx import setup_logger, check_existing_files
 
 sys.path.append("~/Github/fosquant/")
 
@@ -43,12 +46,85 @@ def download(config_file, animals, get_metafile, get_data, overwrite):
         
     logger = setup_logger(config_data["project_dir"])
         
-    if config_data["metafile"]:
+    if get_metafile:
         logger.info("Downloading metafile from remote Azure repo...")
-        path_to_azcopy = config_data["path_to_azcopy"]
-        subprocess.call("{} cp {} {}".format(path_to_azcopy, config_data["metafile"], config_data["project_dir"]), shell=True)    
+        subprocess.run(["azcopy", "cp", config_data["metafile"], config_data["project_dir"]])
+        
+    csv_file = Path(config_data["project_dir"]) / Path(config_data["metafile"]).name
+    logger.info("Reading CSV file... {}".format(csv_file))
+    if not Path(csv_file).exists():
+        logger.info("CSV file cannot be found. Exiting.")
+        sys.exit(2)
+        
+    df = pd.read_csv(csv_file)
+    
+    if animals == "all":
+        animals_to_download = df["animal"].unique()
+    elif animals == "":
+        logger.info("No animals given. Exiting")
+        sys.exit(2)
+    else:
+        animals_to_download = animals.split()
+        
+    # make directory structure
+    path_root = Path(config_data["project_dir"])
 
+    if not path_root.is_dir():
+        logger.info("Project path does not exist. Exiting.")
+        sys.exit(2)
+        
+    for animal in animals_to_download:
+        animal_root_path = path_root / animal
+        animal_raw_path = animal_root_path / "rawdata"
 
+        for path in [animal_root_path, animal_raw_path]:
+            if not path.is_dir():
+                path.mkdir()
+
+        if get_data:
+            if not check_existing_files(animal_raw_path, overwrite):
+                print("exiting")
+                continue
+            
+        row = df[(df["animal"] == animal)]
+        if len(row) == 0:
+            logger.info("{} not in metafile.".format(animal))
+            continue
+        
+        print(row)
+        
+        slide_files = [slide.item() for slide in [row["slide1A"], row["slide1B"], row["slide1C"]] if slide.item() != "none" ]
+        vsi_files = [file if file.endswith(".vsi") else file + ".vsi" for file in slide_files]
+        
+        folder_columns = [col for col in df.columns if 'folder' in col]
+        if len(folder_columns) == 1:
+            remote_folder = row["folder"].item()
+            vsi_paths = [Path(remote_folder) / vsi_file for vsi_file in vsi_files]
+        elif len(folder_columns) == 0:
+            print("no folders given in metafile. Exiting")
+            sys.exit(2)
+        else:
+            if len(folder_columns) != len(slide_files):
+                print("number of folders does not match number of slides. Exiting")
+                sys.exit(2)
+            else:
+                vsi_paths = []
+                for idx, file in enumerate(slide_files):
+                    vsi_paths.append(Path(row[folder_columns[idx]].item()) / vsi_files[idx])
+        
+        for idx, file in enumerate(vsi_paths):
+            vsi_file_remote = file
+            vsi_file_local = animal_raw_path / "{}_{}A.vsi".format(animal, idx+1)
+            
+            logger.debug("{}, {}".format(vsi_file_remote, vsi_file_local))
+
+            vsi_folder_remote_stub = "_{}_".format(file.name.split(".")[0])
+            vsi_folder_remote = file.parent / vsi_folder_remote_stub
+            
+            subprocess.run(["azcopy", "cp", urljoin(config_data["remote"], str(vsi_file_remote)), vsi_file_local])
+            subprocess.run(["azcopy", "cp", urljoin(config_data["remote"], str(vsi_folder_remote)), animal_raw_path, "--recursive=true"])
+            
+            
 if __name__ == "__main__":
     print("processing stuff")
     download()
@@ -59,61 +135,7 @@ if __name__ == "__main__":
 
 
 
-# csv_file = os.path.join(args_dict["project_dir"], os.path.basename(config_data["metafile"]))
-# logger.info("Reading CSV file... {}".format(csv_file))
-# if not os.path.exists(csv_file):
-#     logger.info("CSV file cannot be found. Exiting.")
-#     sys.exit(2)
 
-# df = pd.read_csv(csv_file)
-
-# if args_dict["animals"] == "all":
-#     args_dict["animals"] = df["animal"].unique()
-# elif args_dict["animals"] == "":
-#     logger.info("No animals given. Exiting")
-#     sys.exit(2)
-# else:
-#     args_dict["animals"] = args_dict["animals"].split()
-
-# # make directory structure
-# path_root = args_dict["project_dir"]
-
-# if not os.path.isdir(path_root):
-#     logger.info("Project path does not exist. Exiting.")
-#     sys.exit(2)
-
-# for animal in args_dict["animals"]:
-#     animal_root_path = os.path.join(path_root, animal)
-#     animal_raw_path = os.path.join(animal_root_path, "rawdata")
-
-#     for path in [animal_root_path, animal_raw_path]:
-#         if not os.path.isdir(path):
-#             os.mkdir(path)
-
-#     if args_dict["get_data"]:
-#         if not check_existing_files(animal_raw_path, args_dict["overwrite"]):
-#             print("exiting")
-#             continue
-
-#         row = df[(df["animal"] == animal)]
-#         if len(row) == 0:
-#             logger.info("{} not in metafile.".format(animal))
-#             continue
-
-#         slide_files = [slide.item() for slide in [row["slide1A"], row["slide1B"], row["slide1C"]] if slide.item() != "none" ]
-#         remote_folder = row["folder"].item()
-
-#         logger.info("Downloading files... {}".format(slide_files))
-#         path_to_azcopy = config_data["path_to_azcopy"]
-
-#         for idx, file in enumerate(slide_files):
-#             if not file.endswith(".vsi"):
-#                 file = file + ".vsi"
-                
-#             vsi_file_remote = os.path.join(config_data["remote"], remote_folder, file)
-#             vsi_file_local = os.path.join(animal_raw_path, "{}_{}A.vsi".format(animal, idx+1))
-
-#             logger.debug("{}, {}".format(vsi_file_remote, vsi_file_local))
 
 #             vsi_folder_remote_stub = "_{}_".format(file.split(".")[0])
 #             vsi_folder_remote = os.path.join(config_data["remote"], remote_folder, vsi_folder_remote_stub)
